7a9f2df2eb92dacd1b48c80676a6ed9c3ec8f7de
BielOliiveiira <gabriel_vm@live.com>
2019-09-11 22:23:34 -0300

    Exercicio11

---
+++ dist/exercicio11.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+var exercicio10_1 = require("./exercicio10");
+var cuboN = 2;
+console.log("o cubo \u00E9 : " + exercicio10_1.cubo(cuboN));
---
+++ exercicio11.ts
@@ -0,0 +1,4 @@
+import { cubo } from "./exercicio10";
+
+let cuboN:number = 2;
+console.log(`o cubo é : ${cubo(cuboN)}`)
